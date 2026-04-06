e5bc262f907408cd916453c26c5f21e9b07efa34
Kauan Santos <kauan.s@iccnew.local>
2026-04-06 14:56:36 -0300

    version 1.0

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,31 @@
-// api/proxy.js
 export default async function handler(req, res) {
-  const G_URL = process.env.G_SCRIPT_URL; // A URL ficará escondida no painel do Vercel
-  
+  const G_URL = process.env.G_SCRIPT_URL; 
+
+  if (!G_URL) {
+    return res.status(500).json({ error: "A variavel G_SCRIPT_URL não foi configurada na Vercel." });
+  }
+
+
   const urlParams = new URLSearchParams(req.query).toString();
   const finalUrl = urlParams ? `${G_URL}?${urlParams}` : G_URL;
 
   try {
-    const response = await fetch(finalUrl, {
+    const options = {
       method: req.method,
-      body: req.method === 'POST' ? JSON.stringify(req.body) : undefined,
-    });
+      headers: { 'Content-Type': 'application/json' },
+      redirect: 'follow' 
+    };
+
+    if (req.method === 'POST') {
+      options.body = JSON.stringify(req.body);
+    }
+
+    const response = await fetch(finalUrl, options);
     const data = await response.json();
+
     res.status(200).json(data);
+
   } catch (error) {
-    res.status(500).json({ error: "Erro na conexão com o Banco de Dados" });
+    res.status(500).json({ error: "Falha na conexão", detalhes: error.message });
   }
 }
